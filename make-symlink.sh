b6dfe958ca0aa4d8ff407090bd6b7d5e69b8395c
emirdeliz <emir.marques@condoconta.com>
2022-08-31 21:42:29 -0300

    feat: Work in progress

--- make-symlink.sh
+++ make-symlink.sh
@@ -2,5 +2,4 @@
 # This sh make symlink to vscode root directory for the folders .vscode and .devcontainer.
 # It's used to make the workspace config visible in the vscode.
 
-ln -sf object-detector-image-webassembly/.vscode .vscode && \
 ln -sf object-detector-image-webassembly/.devcontainer .devcontainer
