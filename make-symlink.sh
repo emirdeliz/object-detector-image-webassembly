588034990ed39843154d10a44de536e60768fa50
emirdeliz <emir.marques@condoconta.com>
2022-09-04 14:50:31 -0300

    feat: Work in progress

--- make-symlink.sh
+++ make-symlink.sh
@@ -3,3 +3,5 @@
 # It's used to make the workspace config visible in the vscode.
 
 ln -sf object-detector-image-webassembly/.devcontainer .devcontainer
+ln -sf object-detector-image-webassembly/docker-compose.yml docker-compose.yml
+ln -sf object-detector-image-webassembly/docker-sync.yml docker-sync.yml
