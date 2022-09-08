39dc4fb9443349b353c5ef3e577efecb00c741fe
emirdeliz <emir.marques@condoconta.com>
2022-09-07 22:27:38 -0300

    feat: Work in progress

--- webassembly/build.sh
+++ webassembly/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 rm -rf ../built/web-assembly
 
 sleep 1.5s
@@ -9,7 +10,7 @@ BUILD_DIR="$SCRIPT_DIR"/../built/web-assembly
 echo BUILD_DIR=$BUILD_DIR
 
 mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit
 
 emcmake cmake "$SCRIPT_DIR"
 emmake make clean
